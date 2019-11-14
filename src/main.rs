e7fcabf799514992f952b96b3b8bf6b8621a5acc
Kaisei Yokoyama <yokoyama.kaisei.sm@alumni.tsukuba.ac.jp>
2019-11-14 12:21:15 +0900

    公開

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+
+mod members;
+
+fn main() -> Result<(), Box<Error>>{
+    let members_json = std::fs::File::open("members.json")?;
+    let members : members::Members = serde_json::from_reader(members_json)?;
+
+    println!("{:?}", &members);
+
+    Ok(())
 }
---
+++ src/members.rs
@@ -0,0 +1,49 @@
+use serde::{Serialize, Deserialize};
+
+/// members.json の内容すべて
+#[derive(Serialize,Deserialize,Debug)]
+pub struct Members(Vec<Cluster>);
+
+/// 入部年度ごとのメンバーの集合
+#[derive(Serialize,Deserialize,Debug)]
+#[serde(untagged)]
+pub enum Cluster {
+    /// 現役
+    #[serde(rename_all = "PascalCase")]
+    Active {
+        year: u32,
+        members: Vec<Member>,
+    },
+    /// 卒業生
+    #[serde(rename_all = "PascalCase")]
+    Alumni {
+        title: String,
+        members: Vec<Member>,
+    },
+}
+
+#[derive(Serialize,Deserialize,Debug)]
+#[serde(rename_all = "PascalCase")]
+pub struct Member {
+    name: String,
+    description: String,
+    thumbnail: Option<String>,
+    tags: Option<Vec<String>>,
+    #[serde(rename="SNS")]
+    sns: Option<SNS>,
+}
+
+#[derive(Serialize,Deserialize,Debug)]
+#[serde(rename_all = "PascalCase")]
+pub struct SNS {
+    twitter: Option<String>,
+    github: Option<String>,
+    link: Option<Link>,
+}
+
+#[derive(Serialize,Deserialize,Debug)]
+#[serde(rename_all = "PascalCase")]
+pub struct Link {
+    url: String,
+    title: String,
+}
